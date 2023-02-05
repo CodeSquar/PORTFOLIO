85cd713541a7d4591943b05584fd163ba0cb46a5
lautixon <106138802+lautixon@users.noreply.github.com>
2023-02-04 21:27:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+
+fetch('projects.json')
+  .then(response => response.json())
+  .then(data => {
+    const projectsGrid = document.querySelector(".projects_grid")
+    for (const project of data.projects) {
+      console.log(project.title, project.tag, project.image)
+      projectsGrid.innerHTML += `   
+      <div class="project_item">
+      <img src="images/${project.image}" alt="" class="project_img">
+      <div class="project_info">
+          <div class="project_info_text">
+              <h3 class="project_title">${project.title}</h3>
+              <h4 class="project_tag">${project.tag}</h4>
+          </div>
+          <a class="project_link">Ver demo<div class="project_link_bganim"></div></a>
+      </div>
+  </div>
+      `
+   
+
+
+    }
+  });
+
+
+
+
+
+
+
 const splashScreen = document.getElementById('splash_screen');
 
 splashScreen.addEventListener('animationend', () => {
